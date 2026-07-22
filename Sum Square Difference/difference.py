d9324171a6d87b9247dc2967e7da0510ffd2e9be
SuperNova0206 <konichyoussef@gmail.com>
2026-07-22 01:05:50 +0100

    :( tried larger number first solution [finished], loop [still processing . . .]

--- Sum Square Difference/difference.py
+++ Sum Square Difference/difference.py
@@ -2,7 +2,7 @@
 
 FirstTerm : int = 1
 CommonDifferent : int = 1
-LastTerm : int = 100
+LastTerm : int = 10 ** 120
 
 # number of terms
 NumberOfTerms : int = (LastTerm - FirstTerm + CommonDifferent) // CommonDifferent
@@ -17,3 +17,11 @@ SumPowerArithmeticSeries : int = ((NumberOfTerms * (NumberOfTerms + 1)) // 2 ) *
 # Different formula n(n + 1)(n - 2)(3n + 1) / 12
 DifferentSums = SumPowerArithmeticSeries - SumArithmeticProgression
 print(DifferentSums)
+
+sum_sq : int = 0
+sum_added : int = 0
+for i in range(LastTerm) :
+    sum_added += i
+    sum_sq = sum_sq + i ** 2
+
+print(sum_added ** 2 - sum_sq)
