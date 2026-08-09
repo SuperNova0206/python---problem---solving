6687187163a6c68b441d2f1bda5aebaddd3f35ab
SuperNova0206 <konichyoussef@gmail.com>
2026-08-09 02:09:48 +0100

    :) done but not by me

--- Largest Prime Factor/Prime.py
+++ Largest Prime Factor/Prime.py
@@ -1,44 +1,10 @@
-import math
-from typing import Tuple
+number : int = int(input("Enter the number: "))
+factor : int = 2
+LastFactor : int = 1
 
-
-# gdc 
-def gdc(divisor : int, n) -> int :
-
-    dividend : int = n // divisor
-    reminder : int = n - dividend * divisor
-
-    while not reminder:
-        dividend, reminder = divisor, divisor % reminder
-        if divisor / reminder == 0 and n / reminder == 0:
-            return reminder
-        dividend, reminder = reminder, dividend % reminder
-    return reminder
-
-
-# pollard's rho algorithem
-def PollardRhoAlgorithem(n : int) -> int :
-
-    x : int = 2 ** 2 + 1 % n
-    y = x ** 2 + 1 % n
-
-    reminder = gdc(abs(x - y), n)
-
-    if not n % reminder: 
-        return reminder
-    elif n % reminder:
-        while n % reminder:
-            x = y % n
-            y = x ** 2 + 1 % n
-            reminder = gdc(abs(y))
-
-print(gdc(21, 1885))
-
-
-
-
-
-
-
-
-        
+while number > 1 :
+    if number % factor == 0:
+        LastFactor = factor
+        number //= factor
+    factor += 1
+print(LastFactor)
