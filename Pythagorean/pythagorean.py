d8e8b4f665308b98ccbc9ae41d51b04f2a689e84
SuperNova0206 <konichyoussef@gmail.com>
2026-09-12 22:31:31 +0100

    :) In the process of solving the problem #9 I found that in order to get the production of abc

--- Pythagorean/pythagorean.py
+++ Pythagorean/pythagorean.py
@@ -1,9 +1,5 @@
-class Solution :
-    def __init__(self, a : int, b : int, c : int) :
-        self.a = a if a else False
-        self.b = b if b else False
-        self.c = c if c else False
+# Euclid's parameterization of the Pythagorean triples 
+a : int 
+b : int 
+c : int 
 
-        
-
-    
