cf88fff8150194f15452e98f2c394aed50b10504
SuperNova0206 <konichyoussef@gmail.com>
2026-09-12 20:44:48 +0100

    :) started creating a flixable class deals base on the given data

---
+++ Pythagorean/pythagorean.py
@@ -0,0 +1,9 @@
+class Solution :
+    def __init__(self, a : int, b : int, c : int) :
+        self.a = a if a else False
+        self.b = b if b else False
+        self.c = c if c else False
+
+        
+
+    
